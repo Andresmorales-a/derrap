25a9ed6b650b5b5e13810de2517b8aca9b9761b4
Andresmorales-a <hhcafmp17@gmail.com>
2024-10-01 20:13:18 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('followButton').addEventListener('click', function() {
+    alert('¡Gracias por seguirnos! Mantente atento a nuestras novedades.');
+});
+
+document.getElementById('discountButton').addEventListener('click', function() {
+    alert('¡Aquí tienes un 10% de descuento en tu próxima compra! Usa el código: SKATE10');
+});
+
